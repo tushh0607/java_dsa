ef4d2886b4a188dc4ad53dbb0bcffa71d95e306e
tushh0607 <tushardavnit@gmail.com>
2026-05-12 18:53:37 +0530

    max prod of suubarray

--- 1d_array/q14.java
+++ 1d_array/q14.java
@@ -1,4 +1,26 @@
 //max product subarray 
 public class q14 {
-    
+        public static void main(String[] args) {
+         int[] arr = {2, 3, -2, 4};
+            maxprod(arr);
+        // int result = maxProd(arr);
+
+        // System.out.println("Maximum Product Subarray = " + result);
+
+    }
+    public static void maxprod(int[] arr){
+        int  maxprod=0;
+        int n=arr.length;
+        for(int i=0;i<n;i++){
+            int prod=1;
+            for (int j = i; j < n; j++) {
+                prod*=arr[j];
+                if(prod>maxprod){
+                    maxprod=prod;
+                }
+            }
+            
+        }
+        System.out.println("max prod is "+maxprod);
+    }
 }
