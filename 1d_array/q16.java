e0640e585b01c26f300f16972f9e9fecfa22fa4a
tushh0607 <tushardavnit@gmail.com>
2026-05-12 19:36:13 +0530

    replace with rank

--- 1d_array/q16.java
+++ 1d_array/q16.java
@@ -1,3 +1,54 @@
 public class q16 {
-    
+     static void replaceByRank(int[] arr) {
+
+        int n = arr.length;
+
+        // Copy original array
+        int[] temp = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            temp[i] = arr[i];
+        }
+
+        // Sort temp array
+        for (int i = 0; i < n; i++) {
+
+            for (int j = i + 1; j < n; j++) {
+
+                if (temp[i] > temp[j]) {
+
+                    int t = temp[i];
+                    temp[i] = temp[j];
+                    temp[j] = t;
+                }
+            }
+        }
+
+        // Replace with rank
+        for (int i = 0; i < n; i++) {
+
+            for (int j = 0; j < n; j++) {
+
+                if (arr[i] == temp[j]) {
+
+                    arr[i] = j + 1;
+                    break;
+                }
+            }
+        }
+
+        // Display ranked array
+        System.out.println("Array after replacing by rank:");
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {20, 15, 26, 2, 98, 6};
+
+        replaceByRank(arr);
+    }
 }
