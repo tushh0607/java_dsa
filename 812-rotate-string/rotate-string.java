853ef28a6d8e3221643cd1c802a0629ad751f617
tushh0607 <tushardavnit@gmail.com>
2026-07-21 23:28:57 +0530

    Time: 2 ms (39.38%) | Memory: 43.3 MB (38.83%) - LeetSync

---
+++ 812-rotate-string/rotate-string.java
@@ -0,0 +1,15 @@
+// class Solution {
+//     public boolean rotateString(String s, String goal) {
+//         return (s.length()==goal.length() && (s+s).contains(goal));
+//     }
+// }
+
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        if(s.length() != goal.length()){
+             return false;
+        }
+        return ((s+s).contains(goal));
+    }
+    
+}
