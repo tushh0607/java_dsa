9952ff8c194a3519a48915f9a048de02a1b32382
tushh0607 <tushardavnit@gmail.com>
2026-07-21 15:28:29 +0530

    Time: 0 ms (100.00%) | Memory: 44.8 MB (29.04%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.java
@@ -0,0 +1,32 @@
+// class Solution {
+//     public int searchInsert(int[] nums, int target) {
+//        int n=nums.length;
+//        for(int i=0;i<n;i++){
+//         if(nums[i] == target){
+//             return i; 
+//         }
+//         else if(nums[i]>target){
+//             return i;
+//         }
+//        }
+//        return n;
+//     }
+// }
+
+class Solution{
+    public int searchInsert(int[] nums,int target){
+        int n=nums.length;
+        int low=0;
+        int high=n;
+        while(low<high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]>=target){
+                high=mid;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return low;
+    }
+}
